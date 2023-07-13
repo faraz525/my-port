8d1aed474a8ce1478cd0913ed61dbcf4584be5f2
Faraz Quresui <farazq638@gmail.com>
2023-07-13 17:52:12 -0400

    more

---
+++ components/Entrepreneurship.tsx
@@ -0,0 +1,65 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+import ExperinceCard from './ExperinceCard'
+
+
+type Props = {}
+
+// Margin is to move an element around the screen as it the object with the 
+// other portions of the screen 
+// Padding on the other hand is the spacing internaly of the element
+const Entrepreneurship = (props: Props) => {
+    const internships = [
+        {
+          role: 'Co-Founder & CTO',
+          company: 'Cledge',
+          date: 'December 2020 – Current',
+          p1: 'Launched Cledge to tackle inequity in the college consulting industry and revolutionize college advising.',
+          p2: 'Won #1 at Dempsey Startup Competition, Microsoft for Startups company, IBM Startups, and MassChallange accelerator, reaching a community of 2,000+ parents/students.',
+          p3: 'Directed and collaborated with a team of 12 developers to develop web solutions using Typescript, React, NextJS, and MongoDB',
+          p4: 'Involved in agile framework, design specification, tech integration, product review, and go to market pipeline.',
+          picture: '/cledge.png',
+        },
+        {
+            role: 'Co-Founder & CTO',
+            company: 'Aware Non-profit',
+            date: 'September 2018 – September 2020',
+            p1: 'Launched Cledge to tackle inequity in the college consulting industry and revolutionize college advising.',
+            p2: 'Won #1 at Dempsey Startup Competition, Microsoft for Startups company, IBM Startups, and MassChallange accelerator, reaching a community of 2,000+ parents/students.',
+            p3: 'Directed and collaborated with a team of 12 developers to develop web solutions using Typescript, React, NextJS, and MongoDB',
+            p4: 'Involved in agile framework, design specification, tech integration, product review, and go to market pipeline.',
+            picture: '/tesla_logo_PNG19.png',
+          },
+      ]
+  return (
+    <motion.div 
+        initial={{
+            x:-200,
+            opacity:0,
+        }}
+        whileInView={{
+            opacity: 1,
+            x:0
+        }}
+        transition={{
+            duration:1.2,
+        }}
+        viewport={{
+            once: true
+        }}
+        className='h-screen flex relative flex-col text-lft
+        max-w-full justify-evenly mx-auto items-center overflow-hidden px-10'>
+            <h3 className='uppercase tracking-[20px] text-gray-500 text-2xl mb-5 mt-24'>Startups</h3>
+
+            <div className='scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#800080]/80 w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+                {internships.map((internship, index) => (
+                    <ExperinceCard key={index} {...internship} />
+                ))}
+            </div>
+    </motion.div>
+
+    
+  )
+}
+
+export default Entrepreneurship
