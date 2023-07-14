20cc901b63fd63bf11edf2e5cb2e9aa3e67251da
Faraz Quresui <farazq638@gmail.com>
2023-07-13 22:57:53 -0400

    Testing?

--- components/About.tsx
+++ components/About.tsx
@@ -37,8 +37,8 @@ function About({}: Props) {
         className="flex-shrink-0 mt-24"
         >
 
-        <Image className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
-        md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]' 
+        <Image className='md:-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+            md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]' 
             src="/headshot2.jpeg"
             alt="Picture of the author"
             width={500}
